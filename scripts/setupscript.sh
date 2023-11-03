690779cebf35a550735ecb5b1fb3b56a31808e48
Ethan Bowering <ethanbowering24@gmail.com>
2023-11-03 16:31:10 -0400

    tweaking order

--- scripts/setupscript.sh
+++ scripts/setupscript.sh
@@ -2,23 +2,9 @@
 
 SETUP_REPO=https://raw.githubusercontent.com/KelpieRobotics/2024-underwater-computer/scope-1-ubuntu
 
+set -e
+
 apt-get update && apt-get upgrade -y
-
-systemctl disable snapd.service
-systemctl disable snapd.socket
-systemctl disable snapd.seeded.service
-systemctl disable snap.lxd.activate.service
-systemctl disable apparmor
-systemctl disable snapd.apparmor.service
-systemctl mask snapd.service
-
-systemctl disable iscsi.service
-
-touch /etc/cloud/cloud-init.disabled
-
-echo -e "dtoverlay=disable-wifi\ndtoverlay=disable-bt\n[all]" >> /boot/firmware/config.txt
-
-systemctl disable hciuart
 
 curl -fsSL https://raw.githubusercontent.com/DeepwaterExploration/DWE_OS/main/scripts/install-docker.sh | bash -
 
@@ -33,8 +19,23 @@ curl $REPO/scripts/rov-client.service -o /usr/lib/systemd/system/rov-client.serv
 
 curl $REPO/scripts/60_static_config.yaml -o /etc/netplan/60_static_config.yaml
 
-systectl daemon-reload
+echo -e "dtoverlay=disable-wifi\ndtoverlay=disable-bt\n[all]" >> /boot/firmware/config.txt
+
+systemctl daemon-reload
 systemctl enable rov-client.service
 systemctl start rov-client.service
+
+systemctl disable snapd.service
+systemctl disable snapd.socket
+systemctl disable snapd.seeded.service
+systemctl disable snap.lxd.activate.service
+systemctl disable apparmor
+systemctl disable snapd.apparmor.service
+systemctl mask snapd.service
+
+systemctl disable hciuart
+systemctl disable iscsi.service
+
+touch /etc/cloud/cloud-init.disabled
 
 echo "Done. Upon next reboot system will have ip 192.168.0.20."
