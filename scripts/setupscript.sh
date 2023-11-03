41fdacf55d75fc666651d08db18e77b9e0602e72
Ethan Bowering <ethanbowering24@gmail.com>
2023-11-03 16:59:53 -0400

    done

--- scripts/setupscript.sh
+++ scripts/setupscript.sh
@@ -6,15 +6,7 @@ SETUP_REPO=https://raw.githubusercontent.com/KelpieRobotics/2024-underwater-comp
 
 apt-get update && apt-get upgrade -y
 
-echo 0
-
 curl -fsSL https://raw.githubusercontent.com/DeepwaterExploration/DWE_OS/main/scripts/install-docker.sh | bash -
-
-echo 1
-
-curl -fsSL --output-dir /opt --remote-name-all $SETUP_REPO/clientClass.py $SETUP_REPO/uart.py
-
-echo 2
 
 client_service_file="/usr/lib/systemd/system/rov-client.service"
 if [ -f "$client_service_file" ] ; then
@@ -23,11 +15,9 @@ fi
 
 curl -fsSL $SETUP_REPO/scripts/rov-client.service -o /usr/lib/systemd/system/rov-client.service
 
-echo 3
+curl -fsSL --output-dir /opt --remote-name-all $SETUP_REPO/clientClass.py $SETUP_REPO/uart.py
 
 curl -fsSL $SETUP_REPO/scripts/60_static_config.yaml -o /etc/netplan/60_static_config.yaml
-
-echo 4
 
 echo -e "dtoverlay=disable-wifi\ndtoverlay=disable-bt\n[all]" >> /boot/firmware/config.txt
 
