37af35201ecc809f7138587ae1ad567908be35d9
Ethan Bowering <ethanbowering24@gmail.com>
2023-11-04 18:34:40 -0400

    commit

--- scripts/setupscript.sh
+++ scripts/setupscript.sh
@@ -4,6 +4,10 @@ set -e
 
 DWE_REPO=https://raw.githubusercontent.com/DeepwaterExploration/DWE_OS/main
 
+echo "Installing docker if not already installed"
+docker --version || curl -fsSL https://get.docker.com/ | sh
+systemctl enable docker
+
 # Pull DWE-controls docker image
 echo "Pulling DWE-controls docker image"
 docker pull brandondwe/dwe-controls
@@ -26,7 +30,10 @@ if [ -f "$DWE_SERVICE_FILE" ] ; then
 fi
 curl -fsSL $DWE_REPO/docker/dwe-controls.service -o /usr/lib/systemd/system/dwe-controls.service
 
-KELPIE_REPO=https://raw.githubusercontent.com/KelpieRobotics/2024-underwater-computer/scope-1-dietPi
+KELPIE_REPO=https://raw.githubusercontent.com/KelpieRobotics/2024-underwater-computer/scope-1-ubuntu
+
+echo "Installing pip"
+apt-get install pip
 
 echo "Installing pySerial"
 pip install pyserial
@@ -41,6 +48,9 @@ if [ -f "$ROV_SERVICE_FILE" ] ; then
 fi
 curl -fsSL $KELPIE_REPO/scripts/rov-client.service -o /usr/lib/systemd/system/rov-client.service
 
+echo "Setting static IP"
+curl -fsSL $KELPIE_REPO/scripts/60_static_config.yaml -o /etc/netplan/60_static_config.yaml
+
 systemctl daemon-reload
 
 systemctl enable dwe-controls
@@ -50,3 +60,29 @@ echo "Installation of dwe-controls with docker was successful. Please navigate t
 systemctl enable rov-client.service
 systemctl start rov-client.service
 echo "Installation of ROV client was successful. Attached to 192.168.0.21:9000 with serial port /dev/ttyACM0"
+
+CONFIG_FILE="/boot/firmware/config.txt"
+CONFIG_WIFI_STRING="dtoverlay=disable-wifi"
+CONFIG_BT_STRING="dtoverlay=disable-bt"
+
+echo "Disabling wifi and bluetooth"
+if ! grep -q "$CONFIG_WIFI_STRING" "$CONFIG_FILE"; then
+    echo "$CONFIG_WIFI_STRING" >> "$CONFIG_FILE"
+fi
+
+if ! grep -q "$CONFIG_BT_STRING" "$CONFIG_FILE"; then
+    echo "$CONFIG_BT_STRING" >> "$CONFIG_FILE"
+fi
+
+if ! [ $(tail -n 1 "$CONFIG_FILE") = "[all]" ] ; then
+    echo "[all]" >> "$CONFIG_FILE"
+fi
+
+systemctl disable hciuart
+
+echo "Disabling services to speed up boot"
+
+systemctl disable wpa_supplicant.service
+
+
+echo "Done. Upon next reboot system will have ip 192.168.0.20."
