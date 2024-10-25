7fb4c13aa1a9f1f1c866d80f453ac18c53af7785
yahyaosman <lykan622@gmail.com>
2024-10-24 22:10:45 -0400

    udpated start-gstreamer.sh, added systemd-unit-files folder, and made a start-gstreamer unit file. start-gstreamer.sh script works, unit file needs to be tested

---
+++ scripts/start-gstreamer.sh
@@ -0,0 +1,15 @@
+CAMERA1= "/dev/video1"
+CAMERA2= "/dev/video2"
+CAMERA3= "/dev/video3"
+WIDTH=1920
+HEIGHT=1080
+
+#to topside (non-wsl)
+gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264,width=$WIDTH, height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5601 sync=false &
+gst-launch-1.0 -v v4l2src device=$CAMERA2 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5602 sync=false &
+gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264, width=$WIDTH,height=$$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5600 sync=false &
+
+#broadcast
+gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5601 sync=false
+gst-launch-1.0 -v v4l2src device=$CAMERA2 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5602 sync=false
+gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5600 sync=false
---
+++ systemd-unit-files/start-gstreamer-&-cameras/README.md
@@ -0,0 +1,15 @@
+# systemd-unit-files
+
+systemd unit files for:
+
+- Automatically starting '''start-gstreamer.sh''' script, for camera feed.
+
+## configuration
+
+Nothing right now to configure
+
+## installation
+
+```
+# make install
+```
