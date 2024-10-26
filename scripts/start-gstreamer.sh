abf15d4dd4cc0e9fcf7769ec69805ce7190f1ddf
yahyaosman <lykan622@gmail.com>
2024-10-26 12:35:10 -0400

    Updated spacing in start-gstreamer script

--- scripts/start-gstreamer.sh
+++ scripts/start-gstreamer.sh
@@ -5,11 +5,11 @@ WIDTH=1920
 HEIGHT=1080
 
 #to topside (non-wsl)
-gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264,width=$WIDTH, height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5601 sync=false &
+gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5601 sync=false &
 gst-launch-1.0 -v v4l2src device=$CAMERA2 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5602 sync=false &
-gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264, width=$WIDTH,height=$$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5600 sync=false &
+gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.1 port=5600 sync=false &
 
 #broadcast
-gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5601 sync=false
-gst-launch-1.0 -v v4l2src device=$CAMERA2 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5602 sync=false
-gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264, width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5600 sync=false
+gst-launch-1.0 -v v4l2src device=$CAMERA1 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5601 sync=false
+gst-launch-1.0 -v v4l2src device=$CAMERA2 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5602 sync=false
+gst-launch-1.0 -v v4l2src device=$CAMERA3 ! video/x-h264,width=$WIDTH,height=$HEIGHT ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.137.255 port=5600 sync=false
